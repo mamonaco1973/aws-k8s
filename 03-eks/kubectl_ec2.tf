1b2c463a449d654c11f3bed6d8bb2684dc1d3dce
Mike Monaco <mamonaco1973@gmail.com>
2025-03-17 09:26:43 -0400

    remove ec2

--- 03-eks/kubectl_ec2.tf
+++ 03-eks/kubectl_ec2.tf
@@ -1,51 +1,51 @@
-# EC2 INSTANCE CONFIGURATION
-# This resource block defines an AWS EC2 instance named "kubectl_instance".
+# # EC2 INSTANCE CONFIGURATION
+# # This resource block defines an AWS EC2 instance named "kubectl_instance".
 
-resource "aws_instance" "kubectl_instance" {
+# resource "aws_instance" "kubectl_instance" {
   
-  ami = data.aws_ami.ubuntu_ami.id
+#   ami = data.aws_ami.ubuntu_ami.id
 
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.k8s-subnet-1.id
+#   instance_type = "t2.micro"
+#   subnet_id = aws_subnet.k8s-subnet-1.id
 
-  vpc_security_group_ids = [
-    aws_security_group.ad_ssm_sg.id
-  ]
+#   vpc_security_group_ids = [
+#     aws_security_group.ad_ssm_sg.id
+#   ]
 
-  associate_public_ip_address = true
-  iam_instance_profile = aws_iam_instance_profile.eks_kubectl_instance_profile.name
+#   associate_public_ip_address = true
+#   iam_instance_profile = aws_iam_instance_profile.eks_kubectl_instance_profile.name
 
-  user_data = file("./scripts/userdata.sh")
+#   user_data = file("./scripts/userdata.sh")
 
-  tags = {
-    Name = "kubectl-instance"  # The EC2 instance name in AWS.
-  }
-}
+#   tags = {
+#     Name = "kubectl-instance"  # The EC2 instance name in AWS.
+#   }
+# }
 
-resource "aws_iam_role" "eks_kubectl_role" {
-  name = "eks-kubectl-role"
+# resource "aws_iam_role" "eks_kubectl_role" {
+#   name = "eks-kubectl-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "ec2.amazonaws.com"
+#         }
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_policy_attachment" "ssm_policy" {
-  name       = "ssm-policy-attachment"
-  roles      = [aws_iam_role.eks_kubectl_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
+# resource "aws_iam_policy_attachment" "ssm_policy" {
+#   name       = "ssm-policy-attachment"
+#   roles      = [aws_iam_role.eks_kubectl_role.name]
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+# }
 
-resource "aws_iam_instance_profile" "eks_kubectl_instance_profile" {
-  name = "eks-kubectl-instance-profile"
-  role = aws_iam_role.eks_kubectl_role.name
-}
+# resource "aws_iam_instance_profile" "eks_kubectl_instance_profile" {
+#   name = "eks-kubectl-instance-profile"
+#   role = aws_iam_role.eks_kubectl_role.name
+# }
--- 03-eks/security_groups.tf
+++ 03-eks/security_groups.tf
@@ -1,23 +1,23 @@
-# Security Group for SSM (Port 443) - Used for AWS Systems Manager (SSM) agent communication
-resource "aws_security_group" "ad_ssm_sg" {
-  name        = "ad-ssm-security-group"               # Security Group name
-  description = "Allow SSM access from the internet"  # Description of the security group
-  vpc_id      = aws_vpc.k8s-vpc.id                    # Associates the security group with the specified VPC
+# # Security Group for SSM (Port 443) - Used for AWS Systems Manager (SSM) agent communication
+# resource "aws_security_group" "ad_ssm_sg" {
+#   name        = "ad-ssm-security-group"               # Security Group name
+#   description = "Allow SSM access from the internet"  # Description of the security group
+#   vpc_id      = aws_vpc.k8s-vpc.id                    # Associates the security group with the specified VPC
 
-  # INGRESS: Defines inbound rules allowing access to port 443 (HTTPS for SSM communication)
-  ingress {
-    description = "Allow SSM from anywhere"           # This rule permits SSM agent communication from all IPs
-    from_port   = 443                                 # Start of port range (HTTPS default port)
-    to_port     = 443                                 # End of port range (same as start for a single port)
-    protocol    = "tcp"                               # Protocol type (TCP for HTTPS)
-    cidr_blocks = ["0.0.0.0/0"]                       # WARNING: Allows traffic from ANY IP address (highly insecure!)
-  }
+#   # INGRESS: Defines inbound rules allowing access to port 443 (HTTPS for SSM communication)
+#   ingress {
+#     description = "Allow SSM from anywhere"           # This rule permits SSM agent communication from all IPs
+#     from_port   = 443                                 # Start of port range (HTTPS default port)
+#     to_port     = 443                                 # End of port range (same as start for a single port)
+#     protocol    = "tcp"                               # Protocol type (TCP for HTTPS)
+#     cidr_blocks = ["0.0.0.0/0"]                       # WARNING: Allows traffic from ANY IP address (highly insecure!)
+#   }
 
-  # EGRESS: Allows all outbound traffic (default open rule)
-  egress {
-    from_port   = 0                                   # Start of port range (0 means all ports)
-    to_port     = 0                                   # End of port range (0 means all ports)
-    protocol    = "-1"                                # Protocol (-1 means all protocols)
-    cidr_blocks = ["0.0.0.0/0"]                       # Allows outbound traffic to ANY destination
-  }
-}
+#   # EGRESS: Allows all outbound traffic (default open rule)
+#   egress {
+#     from_port   = 0                                   # Start of port range (0 means all ports)
+#     to_port     = 0                                   # End of port range (0 means all ports)
+#     protocol    = "-1"                                # Protocol (-1 means all protocols)
+#     cidr_blocks = ["0.0.0.0/0"]                       # Allows outbound traffic to ANY destination
+#   }
+# }
