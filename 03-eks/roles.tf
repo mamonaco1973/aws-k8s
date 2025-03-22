85b95996780f633e2a65863b1b91ae8b635413b7
Mike Monaco <mamonaco1973@gmail.com>
2025-03-22 11:01:36 -0400

    autoscaling

--- 03-eks/roles.tf
+++ 03-eks/roles.tf
@@ -109,3 +109,28 @@ resource "aws_iam_role" "cluster_autoscaler" {
     ]
   })
 }
+
+resource "aws_iam_policy" "cluster_autoscaler" {
+  name        = "EKSClusterAutoscalerPolicy"
+  description = "Allows Cluster Autoscaler to manage node group ASGs"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeTags",
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup",
+          "ec2:DescribeLaunchTemplateVersions"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
