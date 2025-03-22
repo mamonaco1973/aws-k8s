92ebcae408996ac86963af80f69de9a76a8c00fd
Mike Monaco <mamonaco1973@gmail.com>
2025-03-22 10:41:42 -0400

    add autoscaler role

--- 03-eks/roles.tf
+++ 03-eks/roles.tf
@@ -110,3 +110,26 @@ resource "aws_iam_policy" "cluster_autoscaler" {
     ]
   })
 }
+
+resource "aws_iam_role" "cluster_autoscaler" {
+  name = "EKSClusterAutoscalerRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = module.eks.oidc_provider_arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "${module.eks.oidc_provider_url}:sub" = "system:serviceaccount:kube-system:cluster-autoscaler"
+          }
+        }
+      }
+    ]
+  })
+}
+
