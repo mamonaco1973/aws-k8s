a9e4b07492e3138641623ce6817073fc4460b5f2
Mike Monaco <mamonaco1973@gmail.com>
2025-03-22 11:04:17 -0400

    autoscaling

--- 03-eks/eks.tf
+++ 03-eks/eks.tf
@@ -124,3 +124,13 @@ resource "kubernetes_service_account" "dynamodb_access_sa" {
   }
 }
 
+resource "kubernetes_service_account" "cluster_autoscaler" {
+  metadata {
+    name      = "cluster-autoscaler"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.cluster_autoscaler.arn
+    }
+  }
+}
+
