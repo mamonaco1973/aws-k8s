23a8c473be3d2dc84db6f28942763ed265dec97d
Mike Monaco <mamonaco1973@gmail.com>
2025-03-22 10:18:29 -0400

    add tags

--- 03-eks/eks.tf
+++ 03-eks/eks.tf
@@ -63,6 +63,19 @@ resource "aws_eks_node_group" "flask_api" {
     aws_iam_role_policy_attachment.eks_registry_policy,     # Allow worker nodes to pull images from ECR
     aws_iam_role_policy_attachment.ssm_policy               # Enable access to AWS Systems Manager for logging and monitoring
   ]
+
+    tags = [
+    {
+      key                 = "k8s.io/cluster-autoscaler/enabled"
+      value               = "true"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "k8s.io/cluster-autoscaler/flask-eks-cluster"
+      value               = "owned"
+      propagate_at_launch = true
+    }
+  ]
 }
 
 # Create an IAM Role for DynamoDB Access using IAM Roles for Service Accounts (IRSA)
