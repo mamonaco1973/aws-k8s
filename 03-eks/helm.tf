2682f5c32726cd9d927c4681875623f820d28513
Mike Monaco <mamonaco1973@gmail.com>
2025-03-22 11:37:18 -0400

    try set_string

--- 03-eks/helm.tf
+++ 03-eks/helm.tf
@@ -89,8 +89,8 @@ resource "helm_release" "cluster_autoscaler" {
     value = "least-waste"
   }
 
- set {
-  name  = "extraArgs.node-group-auto-discovery"
-  value = "'asg:tag=k8s.io/cluster-autoscaler/enabled,k8s.io/cluster-autoscaler/flask-eks-cluster'"
+  set_string {
+    name  = "extraArgs.node-group-auto-discovery"
+    value = "asg:tag=k8s.io/cluster-autoscaler/enabled,k8s.io/cluster-autoscaler/flask-eks-cluster"
   }
 }
