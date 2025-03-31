5a1625167ed92848b9dab3c2a2140b4bdedff511
Mike Monaco <mamonaco1973@gmail.com>
2025-03-31 09:24:03 -0400

    resolve private subnets

--- 03-eks/data.tf
+++ 03-eks/data.tf
@@ -38,3 +38,29 @@ data "aws_subnet" "k8s-subnet-2" {
     values = ["k8s-subnet-2"]          # Match the tag value for the desired subnet.
   }
 }
+
+# -----------------------------------------------------------------------------
+# DATA BLOCK: AWS PRIVATE SUBNET 1
+# -----------------------------------------------------------------------------
+# Retrieves information about the first private subnet in the VPC, identified by its "Name" tag.
+# This enables referencing the subnet dynamically without hardcoding its ID.
+data "aws_subnet" "k8s-private-subnet-1" {
+  # FILTER: Criteria for selecting the subnet.
+  filter {
+    name   = "tag:Name"                     # Use the "Name" tag to identify the subnet.
+    values = ["k8s-private-subnet-1"]       # Match the tag value for the desired private subnet.
+  }
+}
+
+# -----------------------------------------------------------------------------
+# DATA BLOCK: AWS PRIVATE SUBNET 2
+# -----------------------------------------------------------------------------
+# Retrieves information about the second private subnet in the VPC, also identified by its "Name" tag.
+# Useful for multi-AZ or high availability deployments that span private subnets.
+data "aws_subnet" "k8s-private-subnet-2" {
+  # FILTER: Define the criteria for selecting the subnet.
+  filter {
+    name   = "tag:Name"                     # Use the "Name" tag to identify the subnet.
+    values = ["k8s-private-subnet-2"]       # Match the tag value for the desired private subnet.
+  }
+}
